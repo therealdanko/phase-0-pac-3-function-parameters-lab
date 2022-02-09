66b02d89a4cf386c6c91ff9e5157058ac5cd297d
Danko Karleusa <dankomatichevich@gmail.com>
2022-02-09 13:38:49 -0800

    commit tree a1fbc806fa1f8572ff1672b1474ca4810ec68daf

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name) {
+    if (name === `${name}`)
+    return (`Hi, my name is ${name}.`)
+}
+
+ function introductionWithLanguage(name, language) {
+     if (name === `${name}`)
+     if (language === `${language}`)
+     return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+ }
+
+ function introductionWithLanguageOptional(name, language = "JavaScript") {
+    if (name === `${name}`)
+    if (language === `${language}`)
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
